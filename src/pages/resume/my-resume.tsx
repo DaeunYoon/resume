4d9935cd8e89fb120ebfa1dfff3fd3728f0388e4
“DaeunYoon” <daa3230@gmail.com>
2023-07-22 12:24:35 +0200

    not sure why form kit key error happens :(

--- src/pages/resume/my-resume.tsx
+++ src/pages/resume/my-resume.tsx
@@ -171,7 +171,10 @@ export default function Home() {
           {(fields, { add, remove }) => (
             <>
               {fields.map((field) => (
-                <div key={field.key} className="w-full flex flex-col gap-1">
+                <div
+                  key={`position-${field.key}`}
+                  className="w-full flex flex-col gap-1"
+                >
                   <Form.Item
                     {...field}
                     label="Name"
@@ -236,7 +239,10 @@ export default function Home() {
           {(fields, { add, remove }) => (
             <>
               {fields.map((field) => (
-                <div key={field.key} className="w-full flex flex-col gap-1">
+                <div
+                  key={`position-${field.key}`}
+                  className="w-full flex flex-col gap-1"
+                >
                   <Form.Item
                     {...field}
                     label="Position"
